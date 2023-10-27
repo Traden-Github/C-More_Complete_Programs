56c61bfbbf0072d915a954f373e42410c9899b7f
Traden <145915101+Traden-Github@users.noreply.github.com>
2023-10-27 21:12:07 +0800

    Update Craps_Practice.c

--- Craps_Practice.c
+++ Craps_Practice.c
@@ -9,62 +9,62 @@ enum Status { WIN, LOSE, CONTINUE };
 int rollDice(void);
 
 int main(void) {
-	int roll = 0;
-	int sum = 0;
-	int point = 0;
-	puts("Welcome to the casino game \"Craps\"!");
-	enum Status gameResult = CONTINUE;
+    int roll = 0;
+    int sum = 0;
+    int point = 0;
+    puts("Welcome to the casino game \"Craps\"!");
+    enum Status gameResult = CONTINUE;
     puts("Enter 1 to roll dice!"); 
-	while(gameResult == CONTINUE) {
-		scanf("%d", &roll);
-	    if(roll == 1){
-		    sum = rollDice();
-		    printf("Player rolled a sum of %d\n", sum);
-		    switch(sum) {
-		        case 7:
-		        case 11:
-		      	    gameResult = WIN;
-			   	    break;
-			    case 2:
-			    case 3:
-			    case 12:
-			        gameResult = LOSE;
-				    break;
-			    default:
-				    printf("%d is now your point\n", sum); 
-				    point = sum;
-			        puts("It's time to prove your point!");
-				    puts("Enter 1 to roll dice!");
-				    break;
+    while(gameResult == CONTINUE) {
+        scanf("%d", &roll);
+	if(roll == 1){
+	    sum = rollDice();
+	    printf("Player rolled a sum of %d\n", sum);
+	        switch(sum) {
+		    case 7:
+		    case 11:
+		        gameResult = WIN;
+		        break;
+		    case 2:
+		    case 3:
+		    case 12:
+			gameResult = LOSE;
+			break;
+		    default:
+			printf("%d is now your point\n", sum); 
+			point = sum;
+			puts("It's time to prove your point!");
+		        puts("Enter 1 to roll dice!");
+			break;
+		}
+		while(gameResult == CONTINUE) {
+                    scanf("%d", &roll);
+		    if(roll == 1) {
+		    	sum = rollDice();
+		    	printf("Player rolled a sum of %d\n", sum);
+		    	if(sum == point) {
+		    		puts("Wow! You proved your point!");
+		    		gameResult = WIN;
+			}
+			else {
+			    puts("Enter 1 to roll dice!");
+			}
 		    }
-		    while(gameResult == CONTINUE) {
-                scanf("%d", &roll);
-		    	if(roll == 1) {
-		    	    sum = rollDice();
-		    	    printf("Player rolled a sum of %d\n", sum);
-		    	    if(sum == point) {
-		    		    puts("Wow! You proved your point!");
-		    		    gameResult = WIN;
-				    }
-				    else {
-					    puts("Enter 1 to roll dice!");
-				    }
-			    }
-			    else{
-		            puts("Oops, please try again!");
-			    }
+		    else{
+		        puts("Oops, please try again!");
+		    }
 	        }
-	    }
-	    else{
-		    puts("Oops, please try again!");
-	    }
+	}
+	else{
+	    puts("Oops, please try again!");
+	}
     }
     if(gameResult == WIN) {
-    	puts("Congradulations! You Won!");
-	}
-	else {
-		puts("Oops! You lost :C");
-	}
+    	    puts("Congradulations! You Won!");
+    }
+    else {
+        puts("Oops! You lost :C");
+    }
 }
 int rollDice(void) {
 	srand(time(NULL));
